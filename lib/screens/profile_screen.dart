9bac40db5330e2778138f095beaf7eb3ff106146
Rawan Adel <110938793+Rawan7544@users.noreply.github.com>
2024-03-16 20:46:10 +0200

    Create profile_screen.dart

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../widgets/profile_header.dart';
+import '../widgets/tab_bar_views.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 229, 228, 228),
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: DefaultTabController(
+        length: 3,
+        child: Column(
+          children: <Widget>[
+            ProfileHeader(),
+            Container(
+              color: Colors.white,
+              child: TabBar(
+                indicatorColor: Color.fromARGB(255, 24, 82, 189),
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: [
+                  Tab(text: 'Posts'),
+                  Tab(text: 'Comments'),
+                  Tab(text: 'About'),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: TabBarViews(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
