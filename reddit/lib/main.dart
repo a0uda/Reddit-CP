2f2c18cd8cb6987c26c58ba55faf2d3a892542cb
jomana1 <jomana.abdelnaim02@eng-st.cu.edu.eg>
2024-04-12 19:59:11 +0200

    m

--- reddit/lib/main.dart
+++ reddit/lib/main.dart
@@ -40,6 +40,12 @@ void main() async {
       ChangeNotifierProvider(
         create: (context) => BannerPictureController(),
       ),
+      ChangeNotifierProvider(
+        create: (context) => FollowerFollowingController(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => EditProfileController(),
+      ),
     ],
     child: MyApp(),
   ));
