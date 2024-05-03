d037471814b0a1ba5830ed5291f69143998a2dfd
HanzadaFayez <hanzada.eldesouky02@eng-st.cu.edu.eg>
2024-05-03 23:01:18 +0300

    .

--- reddit/lib/widgets/comment.dart
+++ reddit/lib/widgets/comment.dart
@@ -126,6 +126,8 @@ class _CommentState extends State<Comment> {
 
   @override
   Widget build(BuildContext context) {
+    String username = userController.userAbout!.username;
+    bool isMyPost = (username == widget.comment.username) ? true : false;
     return Card(
       borderOnForeground: true,
       shadowColor: Colors.white,
@@ -275,71 +277,75 @@ class _CommentState extends State<Comment> {
                                     ],
                                   ),
                                 ),
-                          PopupMenuItem(
-                            value: 2,
-                            child: const Row(
-                              children: [
-                                Icon(Icons.flag),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Text("Edit")
-                              ],
-                            ),
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  var editedTextController =
-                                      TextEditingController(
-                                          text: widget.comment.description);
-                                  return Container(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: SingleChildScrollView(
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: <Widget>[
-                                          Text(
-                                            'Edit your comment',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.deepOrange[400]),
-                                          ),
-                                          TextFormField(
-                                            controller: editedTextController,
-                                            minLines: 10,
-                                            maxLines: null,
-                                            decoration: const InputDecoration(
-                                              border: OutlineInputBorder(),
+                          if (isMyPost)
+                            PopupMenuItem(
+                              value: 2,
+                              child: const Row(
+                                children: [
+                                  Icon(Icons.flag),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text("Edit")
+                                ],
+                              ),
+                              onTap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    var editedTextController =
+                                        TextEditingController(
+                                            text: widget.comment.description);
+                                    return Container(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: SingleChildScrollView(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            Text(
+                                              'Edit your comment',
+                                              style: TextStyle(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold,
+                                                  color:
+                                                      Colors.deepOrange[400]),
                                             ),
-                                          ),
-                                          const SizedBox(height: 10),
-                                          ElevatedButton(
-                                            style: ElevatedButton.styleFrom(
-                                              backgroundColor:
-                                                  Colors.deepOrange[400],
+                                            TextFormField(
+                                              controller: editedTextController,
+                                              minLines: 10,
+                                              maxLines: null,
+                                              decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                              ),
                                             ),
-                                            child: const Text('Save'),
-                                            onPressed: () async {
-                                              await commentService.EditComment(
-                                                  widget.comment.id!,
-                                                  editedTextController.text);
-                                              setState(() {
-                                                widget.comment.description =
-                                                    editedTextController.text;
-                                              });
-                                              Navigator.pop(context);
-                                            },
-                                          ),
-                                        ],
+                                            const SizedBox(height: 10),
+                                            ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                backgroundColor:
+                                                    Colors.deepOrange[400],
+                                              ),
+                                              child: const Text('Save'),
+                                              onPressed: () async {
+                                                await commentService
+                                                    .EditComment(
+                                                        widget.comment.id!,
+                                                        editedTextController
+                                                            .text);
+                                                setState(() {
+                                                  widget.comment.description =
+                                                      editedTextController.text;
+                                                });
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  );
-                                },
-                              );
-                            },
-                          ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
                         ],
                       );
                     },
