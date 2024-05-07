703a1daa8ff146415ed99784eb55f8d4100270d8
HanzadaFayez <hanzada.eldesouky02@eng-st.cu.edu.eg>
2024-05-08 01:17:55 +0300

    disconnect done

--- reddit/lib/widgets/connect_google_tile.dart
+++ reddit/lib/widgets/connect_google_tile.dart
@@ -13,146 +13,154 @@ class ConnectGoogleTile extends StatefulWidget {
 
 class _ConnectGoogleTileState extends State<ConnectGoogleTile> {
   final UserController userController = GetIt.instance.get<UserController>();
-  // Future<AccountSettings>? accountSettings = userController!.getAccountSettings(userController!.userAbout!.username);
-
   @override
   Widget build(BuildContext context) {
-    bool isConnected = userController.accountSettings!.connectedGoogle;
-    return CustomSettingsTile(
-      title: 'Google',
-      leading: Image.asset(
-        'images/google-icon.png',
-        width: 22,
-        height: 22,
-      ),
-      trailing: GestureDetector(
-        child: Text(
-          isConnected ? 'Disconnect' : 'Connect',
-          style: TextStyle(
-              color: Colors.blue[900],
-              fontSize: 14.0,
-              fontWeight: FontWeight.bold),
-        ),
-        onTap: () async {
-          bool newConnectionStatus = !isConnected;
-          bool connectStatus;
-          int disconnectStatus;
-          if (newConnectionStatus) {
-            connectStatus = await userController
-                .connectToGoogle(userController.userAbout!.username);
-            if (connectStatus) {
-              setState(() {
-                isConnected = newConnectionStatus;
-              });
-            }
+    return FutureBuilder<AccountSettings>(
+        future: userController
+            .getAccountSettings(userController.userAbout!.username),
+        builder:
+            (BuildContext context, AsyncSnapshot<AccountSettings> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
           } else {
-            final TextEditingController passwordController =
-                TextEditingController();
-            await showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Enter your password'),
-                  content: TextField(
-                    controller: passwordController,
-                    style: const TextStyle(
-                      color: Colors.black,
-                    ),
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: "Password",
-                      labelStyle: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  actions: <Widget>[
-                    TextButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.resolveWith<Color?>(
-                          (Set<MaterialState> states) {
-                            return Colors.deepOrange[400];
-                          },
-                        ),
-                      ),
-                      child: const Text('Submit'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
+            bool isConnected = snapshot.data!.connectedGoogle;
+            return CustomSettingsTile(
+              title: 'Google',
+              leading: Image.asset(
+                'images/google-icon.png',
+                width: 22,
+                height: 22,
+              ),
+              trailing: GestureDetector(
+                child: Text(
+                  isConnected ? 'Disconnect' : 'Connect',
+                  style: TextStyle(
+                      color: Colors.blue[900],
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                onTap: () async {
+                  bool newConnectionStatus = !isConnected;
+                  bool connectStatus;
+                  int disconnectStatus;
+                  if (newConnectionStatus) {
+                    connectStatus = await userController
+                        .connectToGoogle(userController.userAbout!.username);
+                    if (connectStatus) {
+                      setState(() {
+                        isConnected = newConnectionStatus;
+                      });
+                    }
+                  } else {
+                    final TextEditingController passwordController =
+                        TextEditingController();
+                    await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Enter your password'),
+                          content: TextField(
+                            controller: passwordController,
+                            style: const TextStyle(
+                              color: Colors.black,
+                            ),
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: "Password",
+                              labelStyle: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color?>(
+                                  (Set<MaterialState> states) {
+                                    return Colors.deepOrange[400];
+                                  },
+                                ),
+                              ),
+                              child: const Text('Submit'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
                       },
-                    ),
-                  ],
-                );
-              },
+                    );
+                    String password = passwordController.text;
+
+                    disconnectStatus =
+                        await userController.disconnectFromGoogle(
+                            userController.userAbout!.username, password);
+
+                    if (disconnectStatus == 2) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Error'),
+                            content: const Text(
+                                "Your don't have password, Add a password to your account first please."),
+                            actions: <Widget>[
+                              TextButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.resolveWith<Color?>(
+                                    (Set<MaterialState> states) {
+                                      return Colors.deepOrange[400];
+                                    },
+                                  ),
+                                ),
+                                child: const Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else if (disconnectStatus == 200) {
+                      setState(() {
+                        isConnected = newConnectionStatus;
+                      });
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Error'),
+                            content: const Text("Cannot disconnect."),
+                            actions: <Widget>[
+                              TextButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.resolveWith<Color?>(
+                                    (Set<MaterialState> states) {
+                                      return Colors.deepOrange[400];
+                                    },
+                                  ),
+                                ),
+                                child: const Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  }
+                },
+              ),
+              onTap: () {},
             );
-            String password = passwordController.text;
-
-            disconnectStatus = await userController.disconnectFromGoogle(
-                userController.userAbout!.username, password);
-
-            if (disconnectStatus == 2) {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Error'),
-                    content: const Text(
-                        "Your don't have password, Add a password to your account first please."),
-                    actions: <Widget>[
-                      TextButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.resolveWith<Color?>(
-                            (Set<MaterialState> states) {
-                              return Colors.deepOrange[400];
-                            },
-                          ),
-                        ),
-                        child: const Text('OK'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            } else if (disconnectStatus == 200) {
-              setState(() {
-                isConnected = newConnectionStatus;
-              });
-            } else {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Error'),
-                    content: const Text("Cannot disconnect."),
-                    actions: <Widget>[
-                      TextButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.resolveWith<Color?>(
-                            (Set<MaterialState> states) {
-                              return Colors.deepOrange[400];
-                            },
-                          ),
-                        ),
-                        child: const Text('OK'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            }
           }
-        },
-      ),
-      onTap: () {
-        // Navigate to the Connect Google Screen
-        // Navigator.of(context).pushNamed('/connect-google');
-      },
-    );
+        });
   }
 }
